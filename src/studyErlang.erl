9e49ba05b48661c3276dcbd73f82ee60b1211888
Everglow <wb-lx936926@alibaba-inc.com>
2021-12-07 16:43:30 +0800

    匹配模式

--- src/studyErlang.erl
+++ src/studyErlang.erl
@@ -11,32 +11,47 @@
 
 %% API
 -export([start/0
-  ,while/1
-  ,while/2
+  , while/1
+  , while/2
 %%  ,for/2
+  , total/1
+  , cost/1
 ]).
 
 %% this project Entrance method
-start()->
+start() ->
   io:fwrite("this is my new study file for log my study process"),
-  X = [1,2,3,4],
+  X = [1, 2, 3, 4],
   while(X),
-  for(5,1)
-.
+  for(5, 1),
+%%  Buy=[{oranges,4},{newspapper,1},{apples,6},{pears,6},{milk,3}],
+  total([{milk, 3}]).
+cost(oranges) -> 4;
+cost(newsspapper) -> 1;
+cost(apples) -> 3;
+cost(pears) -> 6;
+cost(milk) -> 3.
+
+total([{What, N} | T]) ->
+  A = cost(What) * N + total(T),
+%%  打印变量的方法，而不是直接io:fwrite(A)
+  io:fwrite("~w", [A]);
+total([]) -> 0.
+
 
 %% while循环
 %% 所有函数式编程语言，它们不提供任何循环结构
 %% 函数式编程依赖于一种称为递归的概念。
-while(L) -> while(L,0).
+while(L) -> while(L, 0).
 while([], Acc) -> Acc;
-while([_|T], Acc) ->
-  io:fwrite("~w~n",[Acc]),
-  while(T,Acc+1).
+while([_ | T], Acc) ->
+  io:fwrite("~w~n", [Acc]),
+  while(T, Acc + 1).
 
-for(0,_)->
+for(0, _) ->
   [];
-  for(N,Term) when N>0->
-    io:fwrite("Hello word ~n"),
-    [Term|for(N-1,Term)].
+for(N, Term) when N > 0 ->
+  io:fwrite("Hello word ~n"),
+  [Term | for(N - 1, Term)].
 
   
