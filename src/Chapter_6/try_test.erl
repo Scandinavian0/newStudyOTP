3bfa0c50d0b96683ff42ef62b2073bd16fbda863
Everglow <wb-lx936926@alibaba-inc.com>
2022-01-06 16:47:06 +0800

    解析MPEG视频，虽然代码有问题，但是还是作为记录和学习

--- src/Chapter_6/try_test.erl
+++ src/Chapter_6/try_test.erl
@@ -10,13 +10,28 @@
 -author("liuxu").
 
 %% API
--export([demo1/0, demo2/0]).
+-export([demo1/0, demo2/0,demo3/0]).
+%% 异常写法
 demo1() ->
   io:format("~w~n", [[catcher(I) || I <- [1, 2, 3, 4, 5]]]).
 %%  [catcher(I)||I<-[1,2,3,4,5]].
+%% 异常的另一种写法
 demo2() ->
 %%  [{I, (catch generate_exception(I))} || I < [1, 2, 3, 4, 5]]
     io:format("~ts~n", ["这样写有问题"]).
+%% 栈跟踪
+%% 在Erlang 21.0中，获取栈异常的新语法是
+%% _:_:Stacktrace ->
+%%      erlang:display(Stacktrace)
+%% 书上写的erlang:get_stacktrace() 只适用于Erlang 20.0以下版本
+demo3()->
+  try
+      generate_exception(5)
+  catch
+      _:_: X ->
+        erlang:display(X),
+        io:format("~w~n",[{X,erlang:display(X)}])
+  end .
 catcher(N) ->
   try
     generate_exception(N) of Val -> {N, normal, Val}
