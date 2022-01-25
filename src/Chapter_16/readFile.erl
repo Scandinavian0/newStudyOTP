2b5285a8432b1790c6670095d6d5a017d3558d9a
Everglow <wb-lx936926@alibaba-inc.com>
2022-01-25 18:30:25 +0800

    读取文件

---
+++ src/Chapter_16/readFile.erl
@@ -0,0 +1,35 @@
+%%%-------------------------------------------------------------------
+%%% @author liuxu
+%%% @copyright (C) 2022, <COMPANY>
+%%% @doc
+%%%
+%%% @end
+%%% Created : 25. 1月 2022 4:44 PM
+%%%-------------------------------------------------------------------
+-module(readFile).
+-author("liuxu").
+
+%% API
+%% 读取文件
+-export([start/0]).
+start() ->
+  Path = "/Users/liuxu/learnCode/erlang- program/newStudyOTP/src/File/data1.dat",
+  Result = read_file(Path),
+  io:format("Result: ~p~n", [Result]),
+  ChangeGroup = file_change_group(Path),
+  io:format("ChangeGroup: ~p~n", [ChangeGroup]),
+  ChangeOwner = file_change_owner(Path),
+  io:format("ChangeOwner: ~p~n", [ChangeOwner])
+%%  ReadResult=file_read_file(Path),
+%%  io:format("ReadResult : ~p~n",ReadResult)
+
+.
+
+read_file(Path) ->
+  file:consult(Path).
+file_change_group(Path)->
+  file:change_group(Path,1).
+file_change_owner(Path)->
+  file:change_group(Path,1).
+%%file_read_file(Path)->
+%%  file:read_file(Path).
